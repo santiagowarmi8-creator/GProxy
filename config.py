190c303a8e569b2a0dff2574d4206a6c7467a55c
santiagowarmi8-creator <santiagowarmi8@gmail.com>
2026-02-06 16:51:25 -0400

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,34 @@
 import os
 
-TOKEN = os.getenv("8481696044:AAGO4EiQew5ojvJF5c4PjAJFda_tF1i0Gm4")
-ADMIN_ID = int(os.getenv("1915349159", "0"))
+# ===============================
+# TELEGRAM
+# ===============================
+
+TOKEN = os.getenv("8481696044:AAGO4EiQew5ojvJF5c4PjAJFda_tF1i0Gm4", "")
+ADMIN_ID = int(os.getenv("GPROXY_ADMIN_ID", "1915349159"))
+
+# ===============================
+# PAGOS
+# ===============================
 
 CUENTA_BANRESERVAS = "4248676174"
 NOMBRE_CUENTA = "YUDITH DOMINGUEZ"
+
+# ===============================
+# PRECIOS
+# ===============================
+
+PRECIO_PRIMERA = 1500
+PRECIO_RENOVACION = 1000
+
+# ===============================
+# DURACION
+# ===============================
+
+DIAS_PROXY = 30
+
+# ===============================
+# RECORDATORIOS
+# ===============================
+
+REMINDER_DAYS = [7, 3, 1, 0]
